08ca9a4423ad776f4ada2421f48341a1edc633f2
Al Hasanul Banna <tutul5800@gmail.com>
2023-05-18 03:12:00 +0600

    read and delete booked data

--- index.js
+++ index.js
@@ -26,22 +26,65 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    // create database and collection starts
     const serviceCollection = client.db("carDoctor").collection("services");
+    const bookingCollection = client.db("carDoctor").collection("bookings");
+    // create database and collection end
 
-    // READ starts
+    // READ starts for service section
     app.get("/services", async (req, res) => {
       const cursor = serviceCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
-    // READ end
+    // READ end for service section
 
+    // READ specific service data starts
     app.get("/services/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await serviceCollection.findOne(query);
       res.send(result);
     });
+    // READ specific service data end
+
+    // bookings CREATE starts
+    // receive data from client side
+    app.post("/bookings", async (req, res) => {
+      const booking = req.body;
+      console.log(booking);
+      // insert or add data to the database
+      const result = await bookingCollection.insertOne(booking);
+      res.send(result);
+    });
+    // bookings CREATE end
+
+    // booked data READ starts
+    app.get("/bookings", async (req, res) => {
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      const cursor = bookingCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    // booked data READ end
+
+    // booked data DELETE starts
+    app.delete("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookingCollection.deleteOne(query);
+      res.send(result);
+    });
+    // booked data DELETE end
+
+    // booked data UPDATE starts
+    app.put("/bookings/:id", async (req, res) => {
+      const updatedBookings = req.body;
+    });
+    // booked data UPDATE end
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
